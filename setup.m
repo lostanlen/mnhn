17cd1419ad9f758d63b74b6ca58037be6d8da495
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-02-20 15:49:39 +0100

    write setup function

---
+++ setup.m
@@ -0,0 +1,28 @@
+% nFilters_per_octave is an integer
+% ROI_duration is the duration in seconds of the region of interest
+% scattering_modulations is a structure with two fields:
+% - scattering_modulations.nTemporal_modulations is an integer
+% - scattering_modulations.nSpectral_modulations (optional) is an integer
+% If scattering_modulations.nSpectral_modulations is absent, the setup performs
+% plain scattering along time. Otherwise, it performs joint time-frequency
+% scattering (see Andén et al. 2015).
+function setup(nFilters_per_octave, ROI_duration, scattering_modulations)
+T = pow2(nextpow2(round(ROI_duration * sample_rate * 0.5)));
+
+% Wavelet transform
+opts{1}.time.nFilters_per_octave = nFilters_per_octave;
+opts{1}.time.T = T;
+opts{1}.time.size = 4*T;
+opts{1}.time.is_chunked = false;
+
+% Time scattering
+opts{2}.time.nFilters_per_octave = 1;
+opts{2}.time.sibling_mask_factor = 2; % controls the inequality j1 < j2
+opts{2}.time.T = T;
+opts{2}.time.gamma_bounds = [1 scattering_modulations.nTemporal_modulations];
+
+% Frequential scattering if required
+if isfield(scattering_modulations, 'nTemporal_modulations')
+    opts{2}.gamma.T = 2^(scattering_modulations.nTemporal_modulations);
+end
+end
