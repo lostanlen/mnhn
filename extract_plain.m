9a6895a6fef668d046154a15245c6380122a07d5
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-02-20 23:42:16 +0100

    update extract_plain

--- extract_plain.m
+++ extract_plain.m
@@ -8,10 +8,9 @@ for band_index = 1:nBands
     nGamma2s = length(S2.data);
     S2_bands{band_index} = cell(1, nGamma2s);
     for gamma2_index = 1:length(S2.data)
-        gamma_range = S2.ranges{1+0}{gamma2_index}(:, gamma_subscript);
+        gamma_range = S2.ranges{1+0}{gamma2_index}(:, 2);
         assert(gamma_range(1)==1);
-        S2_band = S2.data{gamma2_index}(:, ...
-            gamma_start:min(gamma_stop, end));
+        S2_band = S2.data{gamma2_index}(:, gamma_start:min(gamma_stop, end));
         if ~isempty(S2_band)
             S2_bands{band_index}{gamma2_index} = sum(S2_band, 2);
         end
