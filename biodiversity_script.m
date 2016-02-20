e405840e3494674fa588373ee18efdab04d1ef6b
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-02-20 16:42:03 +0100

    update script

--- biodiversity_script.m
+++ biodiversity_script.m
@@ -1,5 +1,5 @@
 %% params in script
-hertz_bands = ...
+band_freqs = ...
     [125 250 ; ...
     200 500 ; ...
     400 1000 ; ...
@@ -9,8 +9,9 @@ hertz_bands = ...
     7200 16000].';
 sample_rate = 44100;
 nFilters_per_octave = 12;
-ROI_duration = 1.0; % in seconds
-scattering_modulations.nTemporal_modulations = 14;
+ROI_duration = 0.5; % in seconds
+clear scattering_modulations;
+scattering_modulations.nTemporal_modulations = 8;
 
 archs = setup( ...
     nFilters_per_octave, ...
@@ -24,22 +25,26 @@ waveform_path = '~/MATLAB/mnhn/test_sound_tropicalforest.wav';
 [waveform, sample_rate] = audioread_compat(waveform_path);
 
 % take left channel
-waveform = waveform(:, 1);
+waveform = waveform(1:10000, 1);
 
-%% chunk
+% chunk
 audio_chunks = chunk(waveform, archs);
 
 %%
 [S, U] = sc_propagate(audio_chunks, archs);
 
 %% get gamma bands
-gamma_min = min(gamma_bands(:));
-gamma_max = max(gamma_bands(:));
-archs{1}.banks{1}.behavior.gamma_bounds = [gamma_min gamma_max];
-[band_refs, gamma_bands] = get_band_refs(archs, hertz_bands, sample_rate);
+band_gammas = get_band_gammas(archs, band_freqs, sample_rate);
 
 %% First-order coefficients
-S1 = S{1+1};
+
+nBands = length(band_gammas);
+for band_index = 1:nBands
+    gamma_start = band_gammas(band_index, 1);
+    gamma_stop = band_gammas(band_index, 2);
+    S1_bands{} = S1.data(:, gamma_start:gamma_stop).';
+end
+
 %% Setup gamma bands
 gamma_bounds = archs{1}.banks{1}.behavior.gamma_bounds;
 min_gamma = max(gamma_bounds(1), 1);
@@ -48,15 +53,15 @@ resolutions = [archs{1}.banks{1}.metas(min_gamma:max_gamma).resolution];
 frequencies = archs{1}.banks{1}.spec.mother_xi * sample_rate * resolutions;
 nGammas = length(frequencies);
 
-nBands = size(hertz_bands, 2);
+nBands = size(band_freqs, 2);
 gamma_bands = zeros(2, nBands);
 for band_index = 1:nBands
     band_min_gamma = (min_gamma - 1) + ...
-        find(frequencies < hertz_bands(2, band_index), 1);
+        find(frequencies < band_freqs(2, band_index), 1);
     band_min_gamma(isempty(band_min_gamma)) = min_gamma;
     gamma_bands(1, band_index) = band_min_gamma;
     band_max_gamma = (min_gamma - 1) + ...
-        find(frequencies > hertz_bands(1, band_index), 1, 'last'); 
+        find(frequencies > band_freqs(1, band_index), 1, 'last'); 
     band_max_gamma(isempty(band_max_gamma)) = max_gamma;
     gamma_bands(2, band_index) = band_max_gamma;
 end
