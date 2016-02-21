5e4c989cbe5cfa48a0fa375248b855f9fb511040
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-02-21 11:44:56 +0100

    unchunk joint scattering

--- unchunk.m
+++ unchunk.m
@@ -9,9 +9,33 @@ if ~iscell(S{1+1}.data)
         S2_node = S{1+2}.data{gamma2_index};
         S2_node = S2_node((1+end/4):(3*end/4), :, :);
         S2_node = reshape(S2_node, ...
-            size(S2_node, 1) * size(S2_node, 2), size(S2_node, 3));
+            size(S2_node, 1) * size(S2_node, 2), ...
+            size(S2_node, 3));
         S{1+2}.data{gamma2_index} = S2_node;
     end
 else
     % unchunk joint scattering
+    % unchunk psi-psi
+    for gamma2_index = 1:length(S{1+2}{1,1}.data)
+        gamma2_node = S{1+2}{1,1}.data{gamma2_index};
+        for gamma_gamma_index = 1:length(gamma2_node)
+            S2_node = gamma2_node{gamma_gamma_index};
+            S2_node = S2_node((1+end/4):(3*end/4), :, :);
+            S2_node = reshape(S2_node, ...
+                size(S2_node, 1) * size(S2_node, 2), ...
+                size(S2_node, 3), ...
+                size(S2_node, 4));
+            gamma2_node{gamma_gamma_index} = S2_node;
+        end
+        S{1+2}{1,1}.data{gamma2_index} = gamma2_node;
+    end
+    % unchunk psi-phi
+    for gamma_gamma_index = 1:length(S{1+2}{1,2}.data)
+        S2_node = S{1+2}{1,2}.data{gamma_gamma_index};
+        S2_node = S2_node((1+end/4):(3*end/4), :, :);
+        S2_node = reshape(S2_node, ...
+            size(S2_node, 1) * size(S2_node, 2), ...
+            size(S2_node, 3));
+    end
+    S{1+2}{1,2}.data{gamma_gamma_index} = S2_node;
 end
