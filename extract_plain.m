509e34c4ab1f01aa1df6f123a68cd54e899ccdf9
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-02-21 12:34:45 +0100

    gamma_subscript in extract_plain

--- extract_plain.m
+++ extract_plain.m
@@ -1,4 +1,5 @@
 function S2_bands = extract_plain(S2, band_gammas)
+gamma_subscript = S2.variable_tree.time{1}.gamma{1}.leaf.subscripts;
 nBands = length(band_gammas);
 S2_bands = cell(1, nBands);
 for band_index = 1:nBands
@@ -7,8 +8,8 @@ for band_index = 1:nBands
     % S2 band
     nGamma2s = length(S2.data);
     S2_bands{band_index} = cell(1, nGamma2s);
-    for gamma2_index = 1:length(S2.data)
-        gamma_range = S2.ranges{1+0}{gamma2_index}(:, 2);
+    for gamma2_index = 1:nGamma2s
+        gamma_range = S2.ranges{1+0}{gamma2_index}(:, gamma_subscript);
         assert(gamma_range(1)==1);
         S2_band = S2.data{gamma2_index}(:, gamma_start:min(gamma_stop, end));
         if ~isempty(S2_band)
