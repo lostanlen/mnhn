ecf958dcfb692db3e2c49b77ae588b5363d8e88a
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2016-02-21 12:46:36 +0100

    concatenate S1_bands and S2_bands

--- biodiversity_script.m
+++ biodiversity_script.m
@@ -12,7 +12,7 @@ nFilters_per_octave = 12;
 ROI_duration = 1.0; % in seconds
 clear scattering_modulations;
 scattering_modulations.nTemporal_modulations = 10;
-%scattering_modulations.nSpectral_modulations = 3;
+scattering_modulations.nSpectral_modulations = 3;
 
 archs = setup( ...
     nFilters_per_octave, ...
@@ -28,7 +28,15 @@ waveform_path = 'test_sound_tropicalforest.wav';
 waveform = waveform(:, 1);
 
 %%
+% Plain scattering is 3x real-time on a single core
+% Joint scattering is 0.2x real-time on a single core
 tic();
 [S1_bands, S2_bands] = invariant_scattering(waveform, archs, ...
     frequency_bands, sample_rate);
 toc();
+
+%% Concatenation
+S_bands = cell(1, length(S1_bands));
+for band_index = 1:length(S1_bands)
+    S_bands{band_index} = cat(2, S1_bands{band_index}, S2_bands{band_index}).';
+end
